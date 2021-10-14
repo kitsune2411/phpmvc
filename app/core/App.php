8bd3bec8676d43227b4e0b6faa6a123d1c4dd83c
kitsune2411 <0kistune24@gmail.com>
2021-10-14 13:38:43 +0800

    routing

--- app/core/App.php
+++ app/core/App.php
@@ -1,7 +1,20 @@
 <?php
-class App {
+class App
+{
     public function __construct()
     {
-        echo "Web MVC Siap Dijalankan!";
+
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    public function parseURL()
+    {
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
