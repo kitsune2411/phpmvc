4297743bfe73c9883c325e8804f17297c3af1a9b
kitsune2411 <0kistune24@gmail.com>
2021-11-05 13:13:41 +0800

    BASE_URL constant static and dinamis

--- app/core/Constants.php
+++ app/core/Constants.php
@@ -1,3 +1,12 @@
 <?php
-define('BASE_URL', "http://localhost/phpmvc/public");
+//BASE_URL static 
+    // define('BASE_URL', "http://localhost/phpmvc/public");
+
+//BASE_URL dinamis
+
+    //method 1
+    // define("BASE_URL","//".trim("{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}","/"));
+    
+    //method 2
+    define("BASE_URL","//".dirname(trim("{$_SERVER['HTTP_HOST']}{$_SERVER['SCRIPT_NAME']}","/")));
 ?> 
